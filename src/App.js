368d394fca732a9c1950c2b820d4760a296a8531
aditirishiraj3008 <aditi1.mitmpl2023@learner.manipal.edu>
2025-03-23 17:17:47 +0530

    basic layout done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
 import "./App.css";
+import NasaImages from "./NasaImages";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
 	const today = new Date();
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    async function fetchAPIData() {
+      const NASA_KEY = process.env.REACT_APP_NASA_API_KEY;
+      console.log('NASA API KEY:', NASA_KEY);
+      const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${NASA_KEY}`;
+      try {
+        const response = await fetch(url);
+        const apiData = await response.json();
+
+        if (apiData.photos && apiData.photos.length > 0) {
+          setData(apiData.photos); 
+          console.log(apiData.photos)
+        } else {
+          console.log('No photos found');
+        }
+      } catch (err) {
+        console.log('Error fetching data:', err.message);
+      } finally {
+        setLoading(false); 
+      }
+    }
+    fetchAPIData()
+  }, [])
 
 	return (
 		<div className="App">
 			<header>
 				<h1>Nasa Images</h1>
 			</header>
-			<main></main>
+			<main>
+				<div className="image-container">
+
+					{data ? (<NasaImages data={data} />) : 
+						<div className='loadingState'>
+							loading images...
+						</div>
+					}
+				</div>
+				<p className="description">
+					This website displays images from the NASA Mars Rover Photos API. NASA is awesome! 
+				</p>
+			</main>
 			<footer>
-				<p>Copyright &copy; {today.getFullYear()} </p>
+				Copyright &copy; {today.getFullYear()}
 			</footer>
 		</div>
 	);
---
+++ src/NasaImages.js
@@ -0,0 +1,42 @@
+const NasaImages = ({ data }) => {
+  if (!data || data.length === 0) {
+    return <h2>No NASA images available</h2>;
+  }
+
+  for (let index = 0; index < data.length; index++) {
+    const photo = data[index];
+    photo.index = index;
+  }
+  let i = 13;
+  const getPhotos = () => {
+      return (data.map((photo) => (
+        <figure key={photo.id} style={{display: photo.index === i ? "block": "none"}}>
+          <img 
+            src={photo.img_src} 
+            alt={`Photo taken by ${photo.camera.name}`} 
+            className="nasa_image"
+          />
+          <figcaption>Image ID: {photo.id}</figcaption>
+          </figure>
+        )))}
+
+  return (
+    
+    <div className="NasaBox">
+      <div className="image-grid">
+        {/* {data.map((photo) => (
+          <img 
+            key={photo.id} 
+            src={photo.img_src} 
+            alt={`Photo taken by ${photo.camera.name}`} 
+            className="nasa_image"
+          />
+        ))} */}
+        {getPhotos()}
+      </div>
+    </div>
+  );
+};
+
+export default NasaImages;
+ 
