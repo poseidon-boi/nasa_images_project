77b6d4c02f8de44a5def28f41fb632aef0e7bbd1
Poseidon <armaan_0517@yahoo.com>
2025-03-23 16:38:20 +0530

    Added header and footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-	return <div className="App"></div>;
+	const today = new Date();
+
+	return (
+		<div className="App">
+			<header>
+				<h1>Nasa Images</h1>
+			</header>
+			<main></main>
+			<footer>
+				<p>Copyright &copy; {today.getFullYear()} </p>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
